8dfb2c1d1a7f3ba0d6390f07ea725344cc294282
LuisGabrielLiscanoLovera <ccidbcomputacion12@gmail.com>
2019-04-30 15:06:58 -0400

    Update ftpmonitor.py

--- ftpmonitor.py
+++ ftpmonitor.py
@@ -7,35 +7,52 @@ from ftplib import FTP
 import shutil
 import logging
 import logging.handlers
-logger=logging.getLogger('Monitoreo de venta')
-#https://www.lawebdelprogramador.com/codigo/Python/2214-Crear-un-fichero-de-log-y-rotarlo-por-tiempo.html
-#generador de log
-logger.setLevel(logging.DEBUG)
-handler = logging.handlers.TimedRotatingFileHandler(filename='log/file.log', when="m", interval=1, backupCount=5)
-formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%d-%m-%y %H:%M:%S')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-#fecha
-today = date.today()
-ayer = today - timedelta(days=1) #Se estrae el calcula de fecha ayer
-try:
-#parametro de configuracion
-    #parametro de configuracion
-    configuracion = cp.ConfigParser()
-    configuracion.read('conf.cfg')
-    carpetaCompartida = configuracion['modoEjecucion']['carpetaCompartida']
-    
-    codigo = configuracion['General']['codigo']
-    numpc = configuracion['General']['numpc']
-    tiempoSlep = int(configuracion['General']['slep'])
-    #codigo_maquina_.txt
-    archivoU0Z = codigo+"_%s_U0Z.txt"%numpc
-    archivoS1 = codigo+"_%s_S1.txt"%numpc
-    modoEjecucion = int(configuracion['modoEjecucion']['modo'])
 
+
+
+
+
+
+
+
+
+#si exsiste los archivo dependiente
+ifExi = lambda archivo:path.exists(archivo)
+
+
+
+try:
+
+    logger    = logging.getLogger('Monitoreo de venta')
+    logger.setLevel(logging.DEBUG)
+    handler   = logging.handlers.TimedRotatingFileHandler(filename='log/file.log', when="m", interval=1, backupCount=5)
+    formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%d-%m-%y %H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    #fecha
+    today = date.today()
+    #ayer  = today - timedelta(days=1) #Se estrae el calcula de fecha 
+ 
+    #parametro de configuracion
+    configuracion   =   cp.ConfigParser()
+    conf            =   'conf.cfg'
+    txtStImp        =   'Stat_Err.txt'
     
-    rutaFtp = configuracion['PathFTP']['rutaFile']
-    logger.info('Archivo U0Z y S1 creado satifactoriamente')
+    if (ifExi(conf)):
+        configuracion.read(conf)
+        carpetaCompartida   = configuracion['modoEjecucion']['carpetaCompartida']
+        modoEjecucion       = int(configuracion['modoEjecucion']['modo'])
+        rutaFtp             = configuracion['PathFTP']['rutaFile']
+        codigo              = configuracion['General']['codigo']
+        numpc               = configuracion['General']['numpc']
+        tiempoSlep          = int(configuracion['General']['slep'])
+        #nombre salida archivo
+        archivoU0Z          = codigo+"_%s_U0Z.txt"%numpc
+        archivoS1           = codigo+"_%s_S1.txt"%numpc
+    
+    else:
+        exit()
+        logger.warning("Aarchivo de conf no se encuentra disponible")
 
 except Exception as e:
     print (e)
@@ -48,35 +65,26 @@ def conexionFTP():
         ftp = FTP('ftp.cocaisystem.com')
         ftp.login('cocaisys','mE]hX9aW6X32b+')
         ftp.cwd(rutaFtp)
-        estatusCftp=True
+        estatusCftp = True
     except Exception as e:
-        print(e)
-        logger.warning('error al conecrar al servidor ftp '+str(e))
-        estatusCftp=False
+        logger.warning('Error al conecrar al servidor ftp '+str(e))
+        estatusCftp  =  False
     return {'ftp':ftp,'estatusCftp':estatusCftp}
 #fecha de ejecucion al archivo de configuracionn rtn string fecha
 
 def stFcfha():
-    global configuracion
-    configuracion.read('conf.cfg')
+    global configuracion,conf
+    configuracion.read(conf)
     configuracion.set('FeEjecucion', 'fecha', str(today))
-    with open("conf.cfg", "w+") as configfile:configuracion.write(configfile);configfile.close()
+    with open(conf, "w+") as configfile:configuracion.write(configfile);configfile.close()
     return configuracion['FeEjecucion']['fecha']
 #activa IntTFHKA.exe para generar los reportes
 def activarIntTFHKA():#retorna bool
-    try:
-        #global ayer,today
-        #if (ayer.day<10 ):dia="0"+str(ayer.day)
-        #else:dia=str(ayer.day)
-        #if (ayer.month<10 ):mes="0"+str(ayer.month)
-        #f_ayer=dia+mes+str(ayer.year)
-        #fecha_ayer = f_ayer+f_ayer
-        #sys("IntTFHKA.exe UploadReportCmd(U2z %s)" % fecha_ayer)
-        #sys("IntTFHKA.exe UploadStatusCmd(S1)")
+    try:        
         sys("IntTFHKA.exe UploadReportCmd(U0z)")
         sys("IntTFHKA.exe UploadStatusCmd(S1)")
         statusIntTFHKA = True
-        logger.info('Programa ejecutado correctamente!')
+        logger.info('Programa IntTFHKA ejecutado correctamente!')
     except Exception as e:
         logger.warning(str(e))
         statusIntTFHKA = False
@@ -84,137 +92,151 @@ def activarIntTFHKA():#retorna bool
 
 def getStatusIntTFHKA():#retorna bool
     try:
-        getStatus = open("Stat_Err.txt", "r+").read()
-        getStatus=getStatus[0:5].strip()
-        if(len(getStatus) == 4):getStatusIntTFHKA = True
-        else:getStatusIntTFHKA = False
+        global txtStImp
+        if ifExi(txtStImp):
+            getSta    = open(txtStImp, "r+").read()
+            getStatus = getSta[0:5].strip()
+            if(len(getStatus)    ==  4):getStatusIntTFHKA   =  True
+            else:getStatusIntTFHKA   = False
+            
+        else:logger.warning(txtStImp+"error archivo")
+    
     except Exception as e:logger.warning(str(e))
     return getStatusIntTFHKA
+
 
 #lectura de los archivos y registro de archivo para el servidor ftp
 #c/wc U0Z
 def cwU0Z():
     try:
         global archivoU0Z
-        U0Z_ftp = open(archivoU0Z, "a+")
-        #contenido_U0Z = U0Z_ftp.rd()
-        final_de_U0Z_ftp = U0Z_ftp.tell()
-        listaU0Z = ['%s'% open("Reporte.txt", "r+").read()]
-        U0Z_ftp.writelines(listaU0Z)
-        U0Z_ftp.seek(final_de_U0Z_ftp)
-        cwU0Z=True
+        
+        if ifExi(archivoU0Z):
+            U0Z_ftp          = open(archivoU0Z, "a+")
+            #contenido_U0Z   = U0Z_ftp.rd()
+            final_de_U0Z_ftp = U0Z_ftp.tell()
+            listaU0Z = ['%s'% open("Reporte.txt", "r+").read()]
+            U0Z_ftp.writelines(listaU0Z)
+            U0Z_ftp.seek(final_de_U0Z_ftp)
+            cwU0Z             = True
+            U0Z_ftp.close()
+        else:
+
+            cwU0Z            = False
+            logger.warning(str(archivoU0Z)+" No existe!")        
     except Exception as e:
         logger.warning(str(e))
-        cwU0Z=False
+        cwU0Z  =  False
+        U0Z_ftp.close()
     return cwU0Z
 
 def cwS1():
     try:
         global archivoS1
-        S1_ftp = open(archivoS1, "a+")
-        #contenido_S1 = S1_ftp.rd()
-        final_de_S1_ftp = S1_ftp.tell()
-        listaS1 = ['%s'% open("Status.txt", "r+").read()]
-        S1_ftp.writelines(listaS1)
-        S1_ftp.seek(final_de_S1_ftp)
-        cwS1=True
-    except Exception as e:logger.warning(str(e));cwS1=False
+        if ifExi(archivoS1):
+            S1_ftp  =  open(archivoS1, "a+")
+            #contenido_S1 = S1_ftp.rd()
+            final_de_S1_ftp = S1_ftp.tell()
+            listaS1         = ['%s'% open("Status.txt", "r+").read()]
+            S1_ftp.writelines(listaS1)
+            S1_ftp.seek(final_de_S1_ftp)
+            cwS1            =  True
+            S1_ftp.close()
+        else:
+            logger.warning(archivoS1 + " No existe!")
+            cwS1  =  False
+    except Exception as e:logger.warning(str(e));S1_ftp.close()
     return cwS1
 #U0Z
 def pubU0Z():
     try:
         global archivoU0Z
         fileU0Z = str(archivoU0Z)
-        file = open(fileU0Z,'rb')
-        if(conexionFTP()['ftp'].storbinary('STOR %s' % fileU0Z, file)):StatusPubU0Z=True
-        else:StatusPubU0Z=True
+        file    = open(fileU0Z,'rb')
+        if(conexionFTP()['ftp'].storbinary('STOR %s' % fileU0Z, file)):StatusPubU0Z  =  True
+        else:StatusPubU0Z  =  False
         file.close()
         logger.info("U0Z publicado al ftp")
         conexionFTP()['ftp'].quit()
-    except Exception as e:logger.warning(str(e))
+    except Exception as e:file.close();logger.warning(str(e))
     return StatusPubU0Z
 
 def pubS1():
     try:
         global archivoS1
-        fileS1 = str(archivoS1)
-        file = open(fileS1,'rb')
-        if(conexionFTP()['ftp'].storbinary('STOR %s' % fileS1, file)):StatusPubS1=True
-        else:StatusPubS1=True
+        fileS1  =  str(archivoS1)
+        file    =  open(fileS1,'rb')
+        if(conexionFTP()['ftp'].storbinary('STOR %s' % fileS1, file)):StatusPubS1  =  True
+        else:StatusPubS1  =  False
+        
         file.close()
         conexionFTP()['ftp'].quit()
         logger.info("S1 publicado al ftp")
-    except Exception as e:logger.warning(str(e))
+    except Exception as e:
+        logger.warning(str(e))
     return StatusPubS1
 
 def copiaCCU0Z():
     try:
         global archivoU0Z,carpetaCompartida
-        fileU0Z = str(archivoU0Z)
+        fileU0Z  =  str(archivoU0Z)
         if (path.exists(fileU0Z)):
             shutil.copy(fileU0Z, carpetaCompartida)
             logger.info("U0z copiado a a la carpeta compartida")
-            StatusPubU0Z=True
-        else:StatusPubU0Z=False
-    except Exception as e:
-        logger.warning(str(e))
-        StatusPubU0Z=False
+            StatusPubU0Z   =  True
+        else:StatusPubU0Z  =  False
+    except Exception as e:logger.warning(str(e))
     return StatusPubU0Z
 
 def copiaCCS1():
     try:
         global archivoS1,carpetaCompartida
-        fileS1 = str(archivoS1)
+        fileS1  =  str(archivoS1)
         if (path.exists(fileS1)):
             shutil.copy(fileS1, carpetaCompartida)
-            StatusPubS1=True
+            StatusPubS1  =  True
             logger.info("S1 copiado a a la carpeta compartida")
-        else:StatusPubS1=False
-    except Exception as e:
-        StatusPubS1=False
-        logger.warning(str(e))
+        else:StatusPubS1 =False
+    except Exception as e:logger.warning(str(e))
     return StatusPubS1
-
-
-
 
 def pubCCU0Z():
     try:
         global archivoU0Z
-        fileU0Z = str(archivoU0Z)
-        file = open(fileU0Z,'rb')
+        fileU0Z  =  str(archivoU0Z)
+        file     =  open(fileU0Z,'rb')
         if (path.exists(fileU0Z)):
-            if(conexionFTP()['ftp'].storbinary('STOR %s' % fileU0Z, file)):StatusPubU0Z=True
-            else:StatusPubU0Z=True
+            if(conexionFTP()['ftp'].storbinary('STOR %s' % fileU0Z, file)):StatusPubU0Z  =  True
+            else:StatusPubU0Z  =  True
             file.close()
             logger.info("U0Z publicado al ftp")
             conexionFTP()['ftp'].quit()
-        else:StatusPubU0Z=False
+        else:StatusPubU0Z  =  False
     except Exception as e:logger.warning(str(e))
     return StatusPubU0Z
 
 def pubCCS1():
     try:
         global archivoS1
-        fileS1 = str(archivoS1)
+        fileS1   =  str(archivoS1)
         if (path.exists(fileS1)):
-            file = open(fileS1,'rb')
-            if(conexionFTP()['ftp'].storbinary('STOR %s' % fileS1, file)):StatusPubS1=True
-            else:StatusPubS1=True
+            file =  open(fileS1,'rb')
+            if(conexionFTP()['ftp'].storbinary('STOR %s' % fileS1, file)):StatusPubS1  = True
+            else:StatusPubS1  =  True
             file.close()
             conexionFTP()['ftp'].quit()
             logger.info("S1 publicado al ftp")
-            StatusPubS1=False
+            StatusPubS1  =  False
     except Exception as e:
-        StatusPubS1=False
+        StatusPubS1  =  False
         logger.warning(str(e))
     return StatusPubS1
 
-def main ():
-    global ayer,today,tiempoSlep,archivoU0Z,archivoS1
+def main():
+    global today,tiempoSlep,archivoU0Z,archivoS1
     slp(tiempoSlep)
     try:
-        enviado =False
+        enviado  =  False
         if (activarIntTFHKA()):
                 logger.info('Programa IntTFHKA activado ')
                 if (getStatusIntTFHKA()):
@@ -223,18 +245,15 @@ def main ():
                         logger.info('Archivo U0Z y S1 crdo satifactoriamente')
                         if conexionFTP()['estatusCftp']:
                             logger.info('conexiona ftp co el servidor en sincronia ')
-
                             if (pubU0Z() and pubS1()):
                                 #conexionFTP()['ftp'].delete(archivoU0Z)
                                 #conexionFTP()['ftp'].delete(archivoS1)
                                 #conexionFTP()['ftp'].retrlines('LIST')
                                 conexionFTP()['ftp'].quit()
                                 logger.critical('archivo publicado al servidor ftp satfactoriamente ')
-                                enviado = True
-                                print("main modo 1 Exito")
-
+                                enviado  = True
                             else:
-                                enviado = False
+                                enviado  = False
                                 print("Error al publicar archivo U0Z y S1")
                         else:print("error de conexion con el servidor ftp ")#podemos intentar hacer otra coso
                     else:print("Error al escribir U0Z y S1")
@@ -246,117 +265,99 @@ def main ():
     return enviado
 
 def mainDos():
-    global ayer,today,tiempoSlep,archivoU0Z,archivoS1
+    global today,tiempoSlep,archivoU0Z,archivoS1
     slp(tiempoSlep)
     try:
-        enviado =False
-        if (copiaCCS1() and copiaCCU0Z()):
-            logger.info('Archivo U0Z y S1 creado satifactoriamente')            
-            logger.info('Archivo U0Z y S1 copiado a la carpeta compartida satifactoriamente')
-            enviado = True            
-        else:
-            enviado = False
-            print("Error al escribir U0Z y S1")
-    except Exception as e:
-        print(e)
-        logger.warning(str(e))
+        enviado  =  False
+        if (activarIntTFHKA()):
+            logger.info('Programa IntTFHKA activado ')
+            if (getStatusIntTFHKA()):
+                logger.critical('impresora conectada satfactoriamente ')
+                if (cwU0Z() and cwS1()):
+                    logger.info('Archivo U0Z y S1 crdo satifactoriamente')
+                    if (copiaCCS1() and copiaCCU0Z()):
+                        logger.info('Archivo U0Z y S1 copiado a la carpeta compartida satifactoriamente')
+                        enviado  =  True            
+                    else:
+                        enviado  =  False
+                        print("Error al escribir U0Z y S1")
+                else:print("Error al escribir U0Z y S1")
+            else:print("Error de conxion con impresora ")           
+        else:print("error de activacion de los archivos exe!")
+    except Exception as e:logger.warning(str(e))
     return enviado
+
 def mainTres():
     global tiempoSlep,archivoU0Z,archivoS1
     slp(tiempoSlep)
     try:
-        enviado =False
+        enviado      =  False
         if (pubCCS1() and pubCCU0Z()):
             logger.info('Archivo U0Z y S1 publicado a la carpeta compartida satifactoriamente')
-            enviado =True
+            enviado  =  True
         else:
-            enviado = False
+            enviado  =  False
             print("Error al copiar archivo U0Z y S1")
     except Exception as e:logger.warning(str(e))
     return enviado
 
 
-def intento():
-    ejecute=1
-    while ejecute > 0:
-        if main():
-            logger.info('Ejecucion exioso al intento{'     + str(ejecute)+'}')
-        else:
-            logger.warning('error en jecucion al intento{' + str(ejecute)+'}')
-        ejecute+=1
-        if ejecute == 5:#numeros de intentos
-            break
-
-#comprobamos el modo de ejecuccion
-def intentoDos():
-    global tiempoSlep
-    ejecute=1
-    while ejecute > 0:
-        if mainDos():
-            logger.info('Ejecucion exioso al intento{'     + str(ejecute)+'}')
-        else:
-            logger.warning('error en jecucion al intento{' + str(ejecute)+'}')
-        ejecute+=1
-        slp(tiempoSlep)
-        if ejecute == 5:#numeros de intentos
-            break
-
-def intentoTres():
-    ejecute=1
-    while ejecute > 0:
-        if mainTres():
-            logger.info('Ejecucion exioso al intento{'     + str(ejecute)+'}')
-        else:
-            logger.warning('error en jecucion al intento{' + str(ejecute)+'}')
-        ejecute+=1
-        slp(tiempoSlep)
-        if ejecute == 5:#numeros de intentos
-            break
 def modoUno():
-    if main():
-        fechaEjecu=stFcfha()
-        while (True):
-            if (str(fechaEjecu)==str(date.today())):pass
-            else:
-                fechaEjecu=date.today()
-                if main():
-                    logger.critical('Archivo U0Z y S1 pulicado con exito! dia '+today+'reporte dia '+ayer)
-                    stFcfha()
-                else:
-                    intento()
-    else:intento()
-
+    fechaEjecu  =  stFcfha()
+    while True:
+        if main():
+            logger.critical('Ejcucion exitoxa!')
+            while (True):
+                if (str(fechaEjecu)  ==  str(date.today())):pass
+                else:fechaEjecu=date.today();stFcfha();break
+        else:logger.warning('Error en jecucion en systema ')
+        
 def modoDos():
-    if mainDos():
-        fechaEjecu=stFcfha()
-        while (True):
-            if (str(fechaEjecu)==str(date.today())):pass
-            else:
-                fechaEjecu=date.today()
-                if mainDos():
-                    logger.critical('Archivo U0Z y S1 pulicado con exito! dia '+today+'reporte dia '+ayer)
-                    stFcfha()
-                else:
-                    intentoDos()
-    else:intentoDos()
-
+    fechaEjecu  =  stFcfha()
+    while (True):
+        if mainDos():
+            logger.critical('Ejcucion exitoxa! modo 2')
+            while True:            
+                if (str(fechaEjecu)  ==  str(date.today())):pass
+                else:fechaEjecu = date.today();stFcfha();break
+        else:logger.warning('Error en jecucion en systema ')
+          
 def modoTres():
-    if mainTres():
-        fechaEjecu=stFcfha()
-        while (True):
-            if (str(fechaEjecu)==str(date.today())):
-                pass
-            else:
-                fechaEjecu=date.today()
-                if mainTres():
-                    logger.critical('Archivo U0Z y S1 pulicado con exito! dia '+today+'reporte dia '+ayer)
-                    stFcfha()
-                else:print("error modo 3")
-    else:intentoTres()
+    fechaEjecu  =  stFcfha()
+    while (True):
+        if mainTres():
+            logger.critical('Ejcucion exitoxa! modo 3')
+            while (True):
+                if (str(fechaEjecu)  ==  str(date.today())):pass
+                else:fechaEjecu  = date.today();stFcfha();break
+        else:logger.warning('Error en jecucion en systema ')
 
-if modoEjecucion ==1:modoUno()
-if modoEjecucion ==2:modoDos()
-if modoEjecucion ==3:modoTres()
+if modoEjecucion == 1:modoUno()
+if modoEjecucion == 2:modoDos()
+if modoEjecucion == 3:modoTres()
 if (modoEjecucion > 3  or  modoEjecucion < 0):
     logger.warning('Error archivo de configuracion')
     exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
